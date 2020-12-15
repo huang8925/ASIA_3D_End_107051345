f889a4039d7b1f43cef6b86891c397e4d39c375b
huang8925 <72430541+huang8925@users.noreply.github.com>
2020-12-15 15:55:50 +0800

    1215

---
+++ 3D Game/Assets/c#/NPC.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "機器人")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "機器人")
+        {
+            playerInArea = false;
+        }
+    }
+
+    private void Dialog()
+    {
+        for(int i = 0; i < data.dialogA.Length; i++)
+        {
+            print(data.dialogA[i]);
+        }
+    }
+}
---
+++ 3D Game/Assets/c#/NPCData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="NPC 資料", menuName = "PATTY/NPC 資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialogA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialogB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialogC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
