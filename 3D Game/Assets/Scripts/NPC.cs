2d4c0880f8c75f748694ff2ae9cfc378c6691adf
huang8925 <72430541+huang8925@users.noreply.github.com>
2020-12-15 22:29:31 +0800

    1215

---
+++ 3D Game/Assets/Scripts/NPC.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "機器人")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "機器人")
+        {
+            playerInArea = false;
+        }
+    }
+
+    private void Dialog()
+    {
+        for(int i = 0; i < data.dialogA.Length; i++)
+        {
+            print(data.dialogA[i]);
+        }
+    }
+}
